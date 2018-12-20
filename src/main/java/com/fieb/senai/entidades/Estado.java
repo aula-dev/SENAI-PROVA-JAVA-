1417d6d0b35df2746c40ca397c86a3ca0413caaa
Hooligan762 <Hooligan7621@gmail.com>
2018-12-20 02:04:29 -0300

    Classe Estado

---
+++ src/main/java/com/fieb/senai/entidades/Estado.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fieb.senai.entidades;
+
+import java.util.Objects;
+
+/**
+ *Classe que Identifica o Estado do Cliente
+ * @author Ismael
+ */
+public class Estado {
+    
+    private Integer id;
+    private String nome;
+    
+    private Cidade Cidade;
+
+    public Estado(Integer id, String nome, Cidade Cidade) {
+        this.id = id;
+        this.nome = nome;
+        this.Cidade = Cidade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cidade getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(Cidade Cidade) {
+        this.Cidade = Cidade;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.nome);
+        hash = 29 * hash + Objects.hashCode(this.Cidade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Estado other = (Estado) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.Cidade, other.Cidade)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
