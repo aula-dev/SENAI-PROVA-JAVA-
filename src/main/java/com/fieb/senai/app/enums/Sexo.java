f5c9934ddb8c3a8fb7fab6249fb81b39631a3d54
Hooligan762 <Hooligan7621@gmail.com>
2018-12-20 01:50:48 -0300

    Classe Enum Sexo

---
+++ src/main/java/com/fieb/senai/app/enums/Sexo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fieb.senai.app.enums;
+
+/**
+ *Classe enum identifica o sexo do Cliente
+ * @author Ismael
+ */
+public enum Sexo {
+   MASCULINO,
+   FEMININO,
+    
+}
